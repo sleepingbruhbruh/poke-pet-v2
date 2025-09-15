0d624463d25f9214a76ac8e1d5b930b3fbb30c35
Tohny2550 <ponlaput555@gmail.com>
2025-09-15 21:45:56 +0700

    feat: llm

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import { chat } from "./chat";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.post("/chat", chat)
 
 const port = Number(process.env.PORT);
 app.listen(port, () => {
